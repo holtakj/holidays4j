fae97327900356b0cfc09bbf642f5a412eb192a4
Juraj Holtak <juraj.holtak@external.t-mobile.at>
2022-01-09 11:52:47 +0100

    cleanup

--- src/test/java/com/holtak/holidays4j/provider/country/AustriaTest.java
+++ src/test/java/com/holtak/holidays4j/provider/country/AustriaTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 class AustriaTest {
 
-    private Austria INSTANCE = new Austria();
+    private final Austria INSTANCE = new Austria();
 
     @Test
     void bla() {
